6b2a0ab823872ddb53a7881284db52dc2b9427b2
jack01234 <1798204267@qq.com>
2018-06-10 09:03:52 +0800

    first time

--- spring-data-web/src/main/java/com/wmli/spring/data/web/SpringDataApplication.java
+++ spring-data-web/src/main/java/com/wmli/spring/data/web/SpringDataApplication.java
@@ -1,4 +1,11 @@
 package com.wmli.spring.data.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataApplication.class,args);
+    }
 }
